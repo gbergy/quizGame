5e1258dbcc97fb84d3b9c79b51b5f7847f903872
greg <greg.goldberg718@gmail.com>
2014-02-11 16:29:52 -0500

    right click works

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	var right = $('#right');
 	var left = $('#left'); 
 	var questions = [q1, q2, q3, q4, q5];
+	var qindex = 0;
 
 	// if (keyup === 13)
 
@@ -18,19 +19,28 @@ $(document).ready(function(){
 	});
 
 	right.on("click", function(){
-		q1.next().show();
+		if (qindex < questions.length - 1) {
+			qindex++;	
+			showHide();
+		}
 	});
 
+	left.on("click", function () {
+
+	})
+
+	 var showHide = function () {
+	 	for (var i = 0; i < questions.length; i++) {
+	 	if (i === qindex) {
+	 		questions[i].show();
+	 	} else {
+	 		questions[i].hide();
+	 	}
+	 }
+	};
 
 
 
-
-	//  var rightclick = function () {
-	//  	for (var i = questions[0]; i <= 4; i++) {
-	//  	show(questions++); 
-	//  }
-	// };
- 	// right.on("click", rightclick());
 // +++++++++++++++++++++++++++++++++++++++++++++++++++
 
 	// right.on("click", function() {
